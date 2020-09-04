7301c6e011aea717b7b44555580df8a832640c64
Ben Brandt <benjamin.j.brandt@gmail.com>
2020-09-03 22:00:17 -0700

    Actualy do the right implementation for 1.3

--- ch1/src/ex1_3.rs
+++ ch1/src/ex1_3.rs
@@ -1,32 +1,52 @@
-use std::ops::Add;
+use std::cmp::Ord;
+use std::ops::{Add, Mul};
 
-/// Sum of three arguments of the same type
-///
-/// # Examples
-/// ```
-/// let sum = ch1::ex1_3::sum_of_three(1, 2, 3);
-/// assert_eq!(6, sum);
-/// let sum = ch1::ex1_3::sum_of_three(1.0, 2.0, 3.0);
-/// assert_eq!(6.0, sum);
-/// ```
-pub fn sum_of_three<T: Add<Output = T>>(a: T, b: T, c: T) -> T {
-    a + b + c
+/// Square of a given number
+fn square<T: Copy + Mul<Output = T>>(x: T) -> T {
+    x * x
 }
 
-/// Takes three numbers as arguments and returns the sum of the squares of the 
-/// two larger numbers.
-///
-/// # Examples
-/// ```
-/// let sum = ch1::ex1_3::sq_sum_largest(1, 2, 3);
-/// assert_eq!(13, sum);
-/// let sum = ch1::ex1_3::sq_sum_largest(1, 1, 1);
-/// assert_eq!(2, sum);
-/// let sum = ch1::ex1_3::sq_sum_largest(1, 2, 2);
-/// assert_eq!(8, sum);
-/// let sum = ch1::ex1_3::sq_sum_largest(1, 1, 2);
-/// assert_eq!(5, sum);
-/// ```
-pub fn sq_sum_largest<T: Add<Output = T>>(a: T, b: T, c: T) -> T {
-    a + b + c
+/// Sum of squares of two inputs
+fn sum_squares<T: Copy + Add<Output = T> + Mul<Output = T>>(x: T, y: T) -> T {
+    square(x) + square(y)
+}
+
+pub fn sum_squares_largest<T: Copy + Ord + Add<Output = T> + Mul<Output = T>>(
+    a: T,
+    b: T,
+    c: T,
+) -> T {
+    if a >= c && b >= c {
+        sum_squares(a, b)
+    } else if b >= a && c >= a {
+        sum_squares(b, c)
+    } else {
+        sum_squares(a, c)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_square() {
+        assert_eq!(super::square(2), 4);
+    }
+
+    #[test]
+    fn test_sum_squares() {
+        assert_eq!(super::sum_squares(1, 2), 5);
+        assert_eq!(super::sum_squares(2, 3), 13);
+    }
+
+    #[test]
+    fn test_sum_squares_largest() {
+        let sum = super::sum_squares_largest(1, 2, 3);
+        assert_eq!(sum, 13);
+        let sum = super::sum_squares_largest(1, 1, 1);
+        assert_eq!(sum, 2);
+        let sum = super::sum_squares_largest(1, 2, 2);
+        assert_eq!(sum, 8);
+        let sum = super::sum_squares_largest(1, 1, 2);
+        assert_eq!(sum, 5);
+    }
 }
