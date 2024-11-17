352a50acba4e1ea307ead15a62155583043881c8
Ben Brandt <benjamin.j.brandt@gmail.com>
2024-11-17 22:59:50 +0100

    Remove unnecessary lint rules

--- ch1/src/lib.rs
+++ ch1/src/lib.rs
@@ -1,17 +1,5 @@
 //! Implementation of SICP exercises in rust.
 
-#![warn(
-    clippy::nursery,
-    clippy::pedantic,
-    future_incompatible,
-    missing_docs,
-    nonstandard_style,
-    rust_2018_compatibility,
-    rust_2018_idioms,
-    rust_2021_compatibility,
-    unused
-)]
-
 pub mod ex1_3;
 pub mod ex1_7;
 pub mod ex1_8;
